6e1d9a62a6b1bf76cf62418fdd5b27c4bc48fc30
Rohan Sobha <r.r.sobha@student.tudelft.nl>
2024-02-07 12:53:08 +0100

    Refactor endpoint submission to include final answer as JSON string

--- streamlit_mods/endpoints.py
+++ streamlit_mods/endpoints.py
@@ -125,7 +125,8 @@ class Endpoints:
             st.stop()
         try:
             session_id_entry = {"sessionId": session_id} if session_id else {}
-            response = requests.post(f"{backend_url}/submit_final_answer", data={**session_id_entry}, json=final_answer)
+            final_answer_entry = {"finalAnswer": json.dumps(final_answer)}
+            response = requests.post(f"{backend_url}/submit_final_answer", data={**session_id_entry, **final_answer_entry})
             json_response = response.json()
             if json_response["error"] != "":
                 raise Exception(json_response["error"])
